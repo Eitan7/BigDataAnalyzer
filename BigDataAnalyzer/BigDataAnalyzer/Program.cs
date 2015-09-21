4c894f557e274b0670064ecf39def303fec7c909
eitan7 <eitank@hotmail.com>
2015-09-21 11:31:15 -0400

    Inital file

---
+++ BigDataAnalyzer/BigDataAnalyzer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigDataAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Terminate with exception {0}", ex.Message);
+            }
+        }
+    }
+}
